f21035c4ad2abe2c690ca7a77042c638a244cf02
avinash_ponugupati <avinash.ponugupati@avios.com>
2023-03-07 00:15:13 +0000

    initial

---
+++ learn.js
@@ -0,0 +1,37 @@
+/*
+* multi line comment
+*/
+
+
+// single line comment
+
+// keywords - variable declarations
+
+// functional scope
+var a; // default value is undefined
+typeof a; // returns data type
+
+// ES6 (ES2015)
+// variable scope
+
+// block scoped
+let a;
+const b = 10; // exceptions 
+// if the variable type is object, you can change the children values
+
+// variable/function hoisting
+// js browser/compile sorts the variables and functions first
+
+// arrow functions (ES6)
+// block scoped
+// concept of `this`
+
+// window
+// document
+
+// object (JSON format - Javascript object notation)
+/*
+{
+    key(string): value (any data type - string, number, object, array, functions)
+}
+*/
