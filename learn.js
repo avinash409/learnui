746a9d4cdc791e4e81472d89a7fb9bb30e44489d
avinash_ponugupati <avinash.ponugupati@avios.com>
2023-03-19 20:00:09 +0000

    changes

--- learn.js
+++ learn.js
@@ -202,6 +202,7 @@ var students = [
     }
 ];
 
+var section1Students = [];
 if( section1Students.every(stu => {
     
     if(stu.age < 10){
@@ -237,3 +238,182 @@ console.log("mappedArray", mappedArray); // filteredArray length <= mainArray
 
 //shallow copy object
 var objB = {...a};
+
+
+var x =2;
+var y;
+switch(x) {
+  case 1: y = x * 1;
+  case 2: y = x * 3;
+  case 5: y = x *5;
+  default: y = x * 10;
+}
+
+console.log('y', y);
+
+parseInt('123abcd'); // NaN
+// isNaN
+
+const timer1 = setTimeout(() => {
+
+}, 100);
+
+clearTimeout(timer1);
+
+const intTimer = setInterval(() => {
+
+}, 100);
+
+clearInterval(timer);
+
+
+// Event Loop
+
+
+var timer = 0;
+
+function doSetTimeout(j) {
+  // var j; local variable (j = i);
+  console.log('do', j);
+  setTimeout(function(){
+    console.log('do timer', j); // i value will taken during run time (10)
+  }, 100);
+}
+
+for(var i=0; i<10; i++) {
+  // for loop i - local variable
+  // console.log('loop', i);
+  setTimeout(function(){
+    console.log('loop timer', i); // i value will taken during run time (10)
+  }, 100);
+  doSetTimeout(i);
+}
+
+// (for loop)
+
+
+// Object creation
+var obj12 = new Object();
+var obj122 = {};
+
+// import {useState} from "react";
+
+function Product(name, price) {
+  this.name = name;
+  this.price= price;
+}
+
+new Product(1, 2); // {name: 1, price: 2};
+
+function Food(name, price) {
+  Product.call(this, name, price);
+
+  this.category = 'food';
+}
+
+new Food(1,2); // {name: 1, price: 2, category: 'food'}
+
+const numbers = [1,2,3,4];
+
+Math.max.call(null, 1,2,3,4);
+Math.max.apply(null, numbers); // Math.max(...numbers);
+
+const obj121 = {
+  x113: 42,
+  getX: function() {
+    return this.x113;
+  }
+}
+
+obj121.getX(); // 42
+
+// class abcd {
+//   constructor(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   }
+
+//   method() {
+
+//   }
+
+//   componentDidMount() {
+
+//   }
+
+//   componentWillMount() {
+
+//   }
+
+//   componentDidChange() {
+
+//   }
+
+//   render() {
+//     <div>{}</div>
+//   }
+
+// }
+
+
+/* Promise */
+
+var myApiPromise = fetch('url');
+
+myApiPromise.then(function(response) {
+  // success callback
+}, function() {
+  // fail callback
+}).catch(function() {
+  // rejected failed handler
+});
+
+var myPromise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    resolve('success');
+  }, 2000)
+});
+
+// async, await
+/** SPA - Single Page Applications */
+
+
+
+/** AJAX */
+// request headers (params, request header payload)
+// request body
+// request method type (OPTIONS, POST(C), GET (R), PATCH(U),PUT(U), DELETE(D)) -> (CRUD operations)
+
+//reponse headers
+//response body
+
+const req = new Request("url");
+const searchParams = new URLSearchParams();
+searchParams.append('key', 'val');
+
+fetch(req);
+
+fetch("url/endpoint", {
+  body: {},
+
+
+  // request headers, request body
+}).then(() => {
+  // success
+}).catch(() => {
+
+});
+
+// 2xx - Success (200)
+// 3xx - success (302 - Data not changed)
+// 4xx - client request error (400 - Bad request, 404 - not found, 403 - forbidden, 401 - unauthorized)
+// 5xx - Internal Server error (500 - code error, 502 - api gateway error)
+
+// window.XMLHttpRequest
+
+// RESTful API calls - CRUD
+// window.fetch -> client request anything from server
+
+// 3rd party libraries - axios ( interceptors )
+
+// Redux (middlewares for FE)
---
+++ readme.md
@@ -0,0 +1,70 @@
+## JavaScript
+- Core Javascript
+    # document methods
+        # getElementById,
+        # querySelector
+        # querySelectorAll
+        # getElementsByClassName
+        # getElementsByTagName
+        # createElement
+        # apppend
+        # appendChild
+        # getAttribute
+        # setAttribute
+    # Operators
+    # String methods
+    # Array methods
+    # Math methods
+    # object methods
+    # for-in
+    # Promises
+	# Event Handling, Event Loop
+	# Advanced/Object Oriented Java Script (Basics of OOJS covered)
+	# JSON (Objects)
+    # API integrations AJAX (Asynchronous Javascript and XML)
+		# ES6
+        - Variables
+        - Operators
+        - Primitive Methods
+        - Classes
+        - Scoping
+        - De-structuring
+        - Maps/Sets
+        - Asynchronous/Await
+        - Error Handling
+        - Common JS/Import
+        - Symbols
+        - Template literals
+        - Spread operator
+        - For-of
+        - Iterrable, Iterators
+
+- React JS
+      # Application Bootstrapping/BoilerPlate setup
+	  # JSX(Shadow DOM)
+	  # Rendering
+	  # Components Props
+	  # States lifecycle management
+	  # Event Handling
+	  # Conditional Rendering
+	  # Lists
+	  # Controlled/Uncontrolled components
+	  # Inheritance
+	  # React Composition
+	  # Refs
+	  # Hoc
+	  # Render Props/Component communication 
+	  # Context api
+	  # Prop types
+	  # Functional Components
+	  # Hooks
+        - UseState
+        - UseEffect
+        - UseReducer
+        - UseCallback
+        - UseMemo
+        - UseRef
+- Unit Testing
+- npm dependencies
+- TypeScript
+- Redux
